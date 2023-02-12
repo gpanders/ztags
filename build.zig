93d036e7ce41ae17bcd1cc4787c9d4c61cc5ffd2
Gregory Anders <greg@gpanders.com>
2023-02-12 13:46:05 -0700

    Remove DiffStep from build.zig

--- build.zig
+++ build.zig
@@ -1,41 +1,5 @@
 const std = @import("std");
 
-const DiffStep = struct {
-    step: std.build.Step,
-    builder: *std.build.Builder,
-    a: std.build.FileSource,
-    b: std.build.FileSource,
-
-    fn init(
-        builder: *std.build.Builder,
-        a: std.build.FileSource,
-        b: std.build.FileSource,
-    ) *DiffStep {
-        const self = builder.allocator.create(DiffStep) catch unreachable;
-        self.* = .{
-            .builder = builder,
-            .a = a.dupe(builder),
-            .b = b.dupe(builder),
-            .step = std.build.Step.init(.custom, "Diff", builder.allocator, make),
-        };
-
-        return self;
-    }
-
-    fn make(step: *std.build.Step) !void {
-        const self = @fieldParentPtr(DiffStep, "step", step);
-        const a_path = self.a.getPath(self.builder);
-        const b_path = self.b.getPath(self.builder);
-        const a = try std.fs.cwd().readFileAlloc(self.builder.allocator, a_path, 20 * 1024 * 1024);
-        const b = try std.fs.cwd().readFileAlloc(self.builder.allocator, b_path, 20 * 1024 * 1024);
-
-        if (std.mem.indexOfDiff(u8, a, b)) |index| {
-            std.debug.print("{s} and {s} differ at byte {}\n", .{ a_path, b_path, index });
-            return error.FilesDiffer;
-        }
-    }
-};
-
 pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
