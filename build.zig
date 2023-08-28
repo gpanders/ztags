a86e6b155cf2c554743ce51ecf5de3b76241822f
Gregory Anders <greg@gpanders.com>
2023-08-28 13:24:30 -0500

    Actually run unit tests with zig build test
    This was lost in the great build.zig refactor of 0.11.

--- build.zig
+++ build.zig
@@ -22,12 +22,14 @@ pub fn build(b: *std.build.Builder) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_tests = b.addTest(.{
+    const unit_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
 
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&exe_tests.step);
+    test_step.dependOn(&run_unit_tests.step);
 }
